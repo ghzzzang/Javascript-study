2d74d6166f22cac83f985d2ef84e2700a99ceadc
Jihoon Kim <ghzzzang@naver.com>
2023-04-25 00:18:10 +0900

    feat: variable

---
+++ variable.js
@@ -0,0 +1,15 @@
+// variable 변수 
+
+// 변수에 접근하기 위한 함수 
+
+// alert(), console.log()
+
+let name = "my";
+let age = 13;
+
+// const 절대로 바뀌지 않는 상수(수정되지 않는다.);
+
+const male = "남자" 
+const female = "여자"
+
+console.log(male);
